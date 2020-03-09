84452e69c9e90749125fb7d7bffc4dfcba438147
Acevedo Cardona Adelaid Lesdeymariet <adelaid.acevedo16@tectijuana.edu.mx>
2020-03-09 03:01:21 -0700

    Update Examen.hs

--- Examen.hs
+++ Examen.hs
@@ -28,8 +28,55 @@ type Grupos = [Grupo]
 listas :: Grupos
 
 listas = 
-  [("Skillet", [("Fall",2005,15,"1:00 hr")], 2000, "Rock Evangelico", [("Juan", "Bateria", 15, 2002, True)], "Loco", ["Tijuana", "Mx"]),
-  ("Skillet2", [("Falli",2005,15,"1:00 hr")], 2001, "Rock Evangeliczo", [("Juan2", "Guitarra" , 15, 2002, False)], "Loco", ["Tijuana", "Mx"])]
+  [
+  ("Horde", 
+  [("Hellig Usvart",1994,12,"40:30"), ("Alive in Oslo",2007,10,"34:44")], 
+  1994, 
+  "Unblack Metal",
+  [("Jayson Sherlock", "Bateria", 49, 1994, False)],
+  "",
+  []),
+  
+  ("Homeshake", 
+  [("Falli",2005,15,"1:00 hr")],
+  2012, 
+  "Hypnagogic Pop", 
+  [("Peter Sagar", "Guitarra" , 29, 2012, True)],
+  "",
+  []),
+
+  ("Burzum", 
+  [("Falli",2005,15,"1:00 hr")],
+  1991, 
+  "Black Metal", 
+  [("Varg Vikernes", "Bajo" , 47, 1991, True)], 
+  "",
+  []),
+  
+  ("Seltzer", 
+  [("Falli",2005,15,"1:00 hr")],
+  2010, 
+  "Folk", 
+  [("Gabriel Marcelo García Rodríguez", "Guitarra" , 31, 2010, True)],
+  "",
+  []),
+
+  ("City and Colour", 
+  [("Falli",2004,15,"1:00 hr")],
+  2004, 
+  "Country Alternativo", 
+  [("Dallas Green", "Guitarra" , 39, 2004, True)],
+  "",
+  []),
+
+  ("Aries", 
+  [("Falli",2004,15,"1:00 hr")],
+  2004, 
+  "Country Alternativo", 
+  [("Isabel Fernández Reviriego", "Teclado" , 40, 2004, True)],
+  "",
+  [])
+  ]
 
 -- Funcion por Año de Debut → Devuelve los Grupos Musicales que Debutaron en el Mismo Año
 
@@ -66,3 +113,13 @@ fgi lista = [(nombreintegrante, instrumentos, edad, añoingreso, status) | (nomb
 fip :: Grupos -> Integrantes
 fip lista = [(nombreintegrante, instrumentos, edad, añoingreso, status) | (nombregrupo, albumes, añodebut, genero, integrantes, touractual, ciudadesdeltour) <- lista, (nombreintegrante, instrumentos, edad, añoingreso, status) <- integrantes, añoingreso > 2000]
 
+
+{- Links:
+   https://es.wikipedia.org/wiki/Horde_(banda)
+   https://es.wikipedia.org/wiki/Homeshake
+   https://es.wikipedia.org/wiki/Burzum
+   https://es.wikipedia.org/wiki/Seltzer_(banda)
+   https://en.wikipedia.org/wiki/Sleep_Party_People
+   https://es.wikipedia.org/wiki/City_and_Colour
+   https://es.wikipedia.org/wiki/Aries_(banda)
+   -}
