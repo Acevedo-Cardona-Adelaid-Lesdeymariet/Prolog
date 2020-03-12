0d1b1038b920e17ff0b2763cfd1def06429c3b83
Acevedo Cardona Adelaid Lesdeymariet <adelaid.acevedo16@tectijuana.edu.mx>
2020-03-12 01:54:31 -0700

    Update Examen.hs

--- Examen.hs
+++ Examen.hs
@@ -130,15 +130,35 @@ listas =
 
   -- Grupo 6
 
-  ("Aries", 
-  [("Falli",2004,15,"1:00 hr")],
-  2004, 
-  "Country Alternativo", 
+  ("Evanescence", 
   [
-  ("Isabel Fernández Reviriego", "Teclado" , 40, 2004, True)
+  ("Fallen",2003,11,"48:55 min"),
+  ("Anywhere But Home",2004,14,"61:47 min"),
+  ("The Open Door",2006,13,"51:29 min"),
+  ("Evanescence",2011,12,"47:15 min"),
+  ("Synthesis",2017,16,"62:17 min")
   ],
-  "",
-  []),
+  1995, 
+  "Metal Gótico", 
+  [
+  ("Amy Lynn Lee", "Piano" , 38, 1995, True),
+  ("Timothy Todd McCord", "Bajo", 40, 2006, True),
+  ("William Martin Hunt", "Bateria", 48, 2007, True),
+  ("William Troy McLawhorn", "Guitarra", 51,2007, True),
+  ("Jennifer Majura Indrasen", "Guitarra", 36, 2015, True),
+  ("Ben Robert Moody", "Guitarra", 39, 1995, False),
+  ("David Hall Hodges", "Teclado", 41, 1999, False),
+  ("John Charles LeCompt", "Guitarra", 46, 2002, False),
+  ("William Gray", "Bateria", 45, 2002, False),
+  ("William James Boyd", "Bajo", 40, 2002, False),
+  ("Terry Balsamo", "Guitarra", 47, 2003, False),
+  ("Robert Maron Grathanine", "Bateria", 45, 1997, False)
+  ],
+  "Worlds Collide Tour",
+  [
+  "Bruselas (Belgica)","Paris (Francia)", "Londres (Reino Unido)","Berlin (Alemania)", "Fráncfort (Alemania)", "Zúrich (Suiza)","Milán (Italia)", "Múnich (Alemania)", "Hamburgo (Alemania)", "Leipzig (Alemania)", "Düsseldorf (Alemania)", "Ámsterdan (Paises Bajos)"
+  ]
+  ),
 
   -- Grupo 7
 
@@ -153,48 +173,128 @@ listas =
   2009, 
   "Hip Hop Alternativo", 
   [
-  ("Tyler Joseph", "Vocalista" , 31, 2009, True),
-  ("","",1,1,True)
+  ("Tyler Robert Joseph", "Vocalista" , 31, 2009, True),
+  ("Joshua William Dun","Bateria",31,2011,True),
+  ("Nick Thomas","Bajo",0,2009,False),
+  ("Chris Salih","Bateria",34,2009,False)
   ],
-  "",
-  []),
+  "The Bandito Tour 2020",
+  [
+  "Ciudad de Mexico (Mexico)","Cincinnati (Ohio - Estados Unidos)","Landgraaf (Holanda)","Scheessel (Alemania)","Neuhausen ob Eck (Alemania)","Dublin (Irlanda)","St. Gallen (Suiza)","Gdynia (Polonia)","Rotselaar (Belgica)","Arras (Francia)","Kyiv (Ucrania)","Madrid (España)","Moscu (Rusia)","Ostrava (Republica Checa)","Cluj-Napoca (Rumania)"
+  ]
+  ),
 
   -- Grupo 8
 
   ("Magic!", 
   [
-  ("Twenty One Pilots",2009,14,"62:00 min"),
-  ("Regional At Best",2011,15,"66:25 min"),
-  ("Vessel",2013,12,"47:44 min"),
-  ("Blurryface",2015,14,"52:17 min"),
-  ("Trench",2018,14,"56:04 min")
+  ("Don't Kill the Magic",2014,11,"42:10 min"),
+  ("Primary Colours",2016,10,"35:16 min"),
+  ("Expectations",2018,13,"42:21 min")
   ],
-  2009, 
-  "Hip Hop Alternativo", 
+  2012, 
+  "Reggae", 
   [
-  ("Isabel Fernández Reviriego", "Teclado" , 40, 2004, True)
+  ("Nasri Tony Atweh","Vocalista",39,2012,True)
+  ("Mark Pellizzer","Guitarra",39,2012,True)
+  ("Ben Spivak","Bajo",37,2012,True)
+  ("Alexander Tanasijczuk","Bateria",34,2012,True)
   ],
-  "",
+  "Ningun Tour Actual",
   []),
 
    -- Grupo 9
 
   ("The Chainsmokers", 
   [
-  ("Twenty One Pilots",2009,14,"62:00 min"),
-  ("Regional At Best",2011,15,"66:25 min"),
-  ("Vessel",2013,12,"47:44 min"),
-  ("Blurryface",2015,14,"52:17 min"),
-  ("Trench",2018,14,"56:04 min")
+  ("Memories...Do Not Open",2017,12,"43:19 min"),
+  ("Sick Boy",2018,10,"32:46 min"),
+  ("World War Joy",2019,10,"33:43 min")
   ],
-  2009, 
-  "Hip Hop Alternativo", 
+  2012, 
+  "Electropop", 
   [
-  ("Isabel Fernández Reviriego", "Teclado" , 40, 2004, True)
+  ("Andrew Taggart","Guitarra" ,30, 2012, True),
+  ("Alex Pall","Piano",34,2012,True),
+  ("Matt McGuire","Bateria",26,2012,True),
+  ("Rhett Bixler","DJ",33,2012,False),
+  ("Tony Ann","Piano",26,2017,False)
   ],
-  "",
-  [])
+  "Ningun Tour Actual",
+  []),
+
+   -- Grupo 10
+
+  ("Imagen Dragons",
+  [
+  ("Speak to Me",2008,5,"19:23 min"),
+  ("Imagine Dragons",2009,5,"20:20 min"),
+  ("Hell and Silence",2010,5,"17:50 min"),
+  ("It's Time",2011,8,"32:01 min"),
+  ("Continued Silence",2012,6,"19:25 min"),
+  ("Nigth Visions",2012,11,"40:10 min"),
+  ("Smoke + Mirrors",2015,13,"50:55 min"),
+  ("Evole",2017,11,"40:12 min"),
+  ("Origins",2018,12,"40:02 min")
+  ],
+  2008,
+  "Pop Rock",
+  [
+	("Dan Reynolds","Voz",32,2008,True),
+  ("Ben McKee","Bajo",31,2009,True),
+  ("Daniel Wayne Sermon","Mandolina",33,2009,True),
+  ("Daniel Platzman","Viola",31,2011,True),
+  ("Andrew Tolman","Bateria",32,2008,False),
+  ("Brittany Tolman","Guitarra",34,2009,False),
+  ("Theresa Flaminio","Piano",33,2011,False),
+  ("Dave Lemke","Bajo",32,2008,False),
+  ("Aurora Florence","Violin",33,2008,False),
+	("Andrew Beck","Guitarra",33,2008,False)
+	],
+  "Ningun Tour Actual",
+  []),
+
+   -- Grupo 11
+
+  ("Within Temptation",
+  [
+  ("Enter",1997,8,"45:00 min"),
+  ("Mother Earth",2000,14,"76:07 min"),
+  ("The Silent Force",2004,13,"46:24 min"),
+  ("The Heart of Everything",2007,11,"57:06 min"),
+  ("The Unforgiving",2011,12,"58:10 min"),
+  ("Hydra",2014,10,"49:16 min"),
+  ("Resist",2019,10,"47:35 min")
+  ],
+  1996,
+  "	Metal Sinfónico",
+	[
+	("Sharon den Adel","Voz",45,1996,True),
+  ("Robert Westerholt","Guitarra",45,1996,True),
+  ("Jeroen van Veen","Bajo",45,1996,True),
+  ("Rudolf Adrianus Jolie","Guitarra",43,2001,True),
+  ("Martijn Spierenburg","Teclado",45,2001,True),
+  ("Mike Coolen","Bateria",44,2011,True),
+  ("Stefan Helleblade","Guitarra",41,2011,True),
+  ("Stephen van Haestregt","Bateria",47,2001,False),
+  ("Ciro Palma","Bateria",0,1998,False),
+	("David Martijn Westerholt","Teclado",40,1996,False),
+  ("Ivar de Graaf", "Bateria", 46, 1996,False),
+  ("Michiel Papenhove", "Guitarra", 44, 1996, False),
+  ("Jelle Bakker", "Guitarra", 33, 2001, False),
+  ("Dennis Leeflang", "Bateria", 40, 1996, False),
+  ("Richard Willemse", "Bateria", 0, 1996, False)
+	],
+  "Worlds Collide Tour",
+  [
+  "Bruselas (Belgica)","Paris (Francia)", "Londres (Reino Unido)","Berlin (Alemania)", "Fráncfort (Alemania)", "Zúrich (Suiza)","Milán (Italia)", "Múnich (Alemania)", "Hamburgo (Alemania)", "Leipzig (Alemania)", "Düsseldorf (Alemania)", "Ámsterdan (Paises Bajos)"
   ]
+  ) 
+  ]
+
+
+fgr :: Grupos -> NombreGrupo -> [Grupo]
+fgr lista n = [(nombregrupo, albumes, añodebut, genero, integrantes, touractual, ciudadesdeltour) | (nombregrupo, albumes, añodebut, genero, integrantes, touractual, ciudadesdeltour) <- lista, nombregrupo == n]
 
 -- Funcion por Año de Debut → Devuelve los Grupos Musicales que Debutaron en el Mismo Año
 
@@ -205,9 +305,6 @@ fad lista n = [(nombregrupo, albumes, añodebut, genero, integrantes, touractual
 
 fia :: Grupos -> Integrantes
 fia lista = [(nombreintegrante, instrumentos, edad, añoingreso, status) | (nombregrupo, albumes, añodebut, genero, integrantes, touractual, ciudadesdeltour) <- lista, (nombreintegrante, instrumentos, edad, añoingreso, status) <- integrantes, status == True]
-
-{-encontrarPorActivo :: Bandas -> BandaNombre -> Integrantes
-encontrarPorActivo lista x =  [(nombre, instrumento, edad, activo, integracion) | (bandaNombre, añoDebut, genero, albumes, integrantes, tourActual, ciudadesDelTour) <- (encontrarBanda lista x), (nombre, instrumento, edad, activo, integracion) <- integrantes, activo == True]-}
 
 -- Funcion de Albumes → Devuelve la informacion del Grupo Musical 
 
